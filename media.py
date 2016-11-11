081ef4b343450bcdb3fb35f697b1f052f824f423
Mohammad Helal <mas_sina@hotmail.com>
2016-11-11 11:27:12 +0300

    The media.py module contains the movie trailers entities

--- media.py
+++ media.py
@@ -1,11 +1,14 @@
-
+import webbrowser
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image_url, trailer_url):
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+
+    def __init__(self, movie_title, movie_storyline, poster_image, youtube_trailer):
         self.title = movie_title
         self.storyline = movie_storyline
-        self.poster_image_url = poster_image_url
-        self.trailer_url = trailer_url
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = youtube_trailer
 
-
-
+    def show_movie_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+        #print "I am within the show_movie_trailer instance function"
 
