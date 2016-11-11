435e3c211848096d76ab249c55068c42d8587fb5
Mohammad Helal <mas_sina@hotmail.com>
2016-11-11 17:44:59 +0300

    Some minor enhancements

--- media.py
+++ media.py
@@ -1,14 +1,11 @@
 import webbrowser
 class Movie():
+    # This is a class variable, that is in the class level definition, and accessed by the class name itself.
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
+    # The construction function used when instantiating a new movie object.
     def __init__(self, movie_title, movie_storyline, poster_image, youtube_trailer):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = youtube_trailer
-
-    def show_movie_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
-        #print "I am within the show_movie_trailer instance function"
-
